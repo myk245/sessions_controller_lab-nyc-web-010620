0bc1545b79c9a644100210bf2aa678a99e45a923
myk245 <myk245@nyu.edu>
2020-02-09 18:35:14 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+# If the user is not logged in, the page should show them a login link.
+# If the user is logged in, the page should say, "hi, #{name}", and provide a logout link.
+  def create
+    # if the params[:name] does not exist or is empty, the user is redirected to a login page
+    if !params[:name] || params[:name].empty?
+      redirect_to '/sessions/new'
+    else
+      # if the user is already logged in, set the session[:name} equal to the params[:name]
+      # then redirect to the root page, which is defined as 'application#welcome'
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/'
+  end
+
+end
